bd857352af8b3dfb6bb38a8e02396b8ae583c2ac
jabiers <jabiers@nate.com>
2016-02-17 17:28:44 +0900

    good sample

---
+++ server.js
@@ -0,0 +1,75 @@
+var cluster = require('cluster'); // Only required if you want the worker id
+var sticky = require('socketio-sticky-session');
+var port = 8000;
+sticky(function() {
+    // This code will be executed only in slave workers
+
+    var app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
+
+    var redis = require('socket.io-redis');
+    io.adapter(redis({ host: 'syno.ml', port: 6379 }));
+
+    app.get('/', function(req, res){
+        res.sendFile(__dirname + '/public/index.html');
+    });
+
+    var nameSpaces = ['/aaaa', '/bbbb'];
+
+    nameSpaces.forEach(function(nsp) {
+        console.log(nsp);
+        var nameSpace = io.of(nsp);
+        nameSpace.on('connection', function(client){
+            client.on("setNickAndRoom", function(nick, fn){
+            	fn({msg : "Hello " + nick.nick});
+            	client.join(nick.room);
+            	Room = nick.room;
+            	client.broadcast.to(Room).json.send({ msg: "Se conecto al room: " + nick.room, nick : nick });
+            });
+
+            client.on('message', function(message, fn){
+                var msg = message; //{ message: [client.sessionId, message] };
+                buffer.push(msg);
+                if (buffer.length > 15)
+                	buffer.shift();
+                client.broadcast.to(Room).json.send(msg);
+                fn(msg);
+            });
+
+            client.on('disconnect', function(){
+            	client.broadcast.to(Room).json.send({ msg: "Se desconecto"});
+            });
+        });
+        nameSpace.emit('waiting', 'everyone!');
+    } );
+
+    var buffer = [];
+    io.sockets.on('connection', function(client){
+    	var Room = "";
+        client.on("setNickAndRoom", function(nick, fn){
+        	fn({msg : "Hello " + nick.nick});
+        	client.join(nick.room);
+        	Room = nick.room;
+        	client.broadcast.to(Room).json.send({ msg: "Se conecto al room: " + nick.room, nick : nick });
+        });
+
+        client.on('message', function(message, fn){
+            var msg = message; //{ message: [client.sessionId, message] };
+            buffer.push(msg);
+            if (buffer.length > 15)
+            	buffer.shift();
+            client.broadcast.to(Room).json.send(msg);
+            fn(msg);
+        });
+
+        client.on('disconnect', function(){
+        	client.broadcast.to(Room).json.send({ msg: "Se desconecto"});
+        });
+
+    });
+
+    return http;
+}).listen(port, function() {
+    console.log('server started on ' + port + ' port');
+});
