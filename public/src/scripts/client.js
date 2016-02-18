79cbe593976abcd4e1f5d27e08f619beee6c6377
jabiers <jabiers@nate.com>
2016-02-18 10:29:31 +0900

    스타일 정리

---
+++ public/src/scripts/client.js
@@ -0,0 +1,4 @@
+var iframe = document.createElement('iframe');
+iframe.style.display = "none";
+iframe.src = 'http://rgchatsocketserver.azurewebsites.net/client2';
+document.body.appendChild(iframe);
