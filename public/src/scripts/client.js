8ee856a044a42b2ff9420ba9ca7e14af951e8904
jabiers <jabiers@nate.com>
2016-02-18 13:04:21 +0900

    url 변경

--- public/src/scripts/client.js
+++ public/src/scripts/client.js
@@ -1,4 +1,4 @@
 var iframe = document.createElement('iframe');
 iframe.style.display = "none";
-iframe.src = 'http://rgchatsocketserver.azurewebsites.net/client2/' + rgchatToken;
+iframe.src = 'http://rgchatsocketserver.azurewebsites.net/client?token=' + rgchatToken;
 document.body.appendChild(iframe);
